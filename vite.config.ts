f0ce21153b8b176020031ad2f4a7c2ff9e81fd04
chitrank2050 <chitrank2050@gmail.com>
2026-03-02 19:51:46 +0530

    adsense

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
-import { VitePluginRadar } from 'vite-plugin-radar'
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+import adsense from 'vite-plugin-adsense';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');   // no `VITE_` prefix filter
@@ -11,6 +12,9 @@ export default defineConfig(({ mode }) => {
         analytics: [
           { id: env.FLOW_BUILDER_GA_ID || '' },
         ],
+      }),
+      adsense({
+        client: env.FLOW_BUILDER_AD_SENSE_ID || '',
       }),
       react(),
     ],
