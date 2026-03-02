c3ca0c2d34ca981666c6b41ead2faa3792b4c834
chitrank2050 <chitrank2050@gmail.com>
2026-03-02 19:37:36 +0530

    Fixed build deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-/// <reference types="vite/client" />
-
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { VitePluginRadar } from 'vite-plugin-radar'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    VitePluginRadar({
-      analytics: [
-        {
-          id: import.meta.env.FLOW_BUILDER_GA_ID || '',
-        }
-      ]
-    }),
-    react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');   // no `VITE_` prefix filter
+
+  return {
+    plugins: [
+      VitePluginRadar({
+        analytics: [
+          { id: env.FLOW_BUILDER_GA_ID || '' },
+        ],
+      }),
+      react(),
+    ],
+  };
+});
